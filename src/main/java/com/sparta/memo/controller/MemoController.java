53428d28d578f250b9322eb7216fe0e2df17794b
songyuheon98 <songyuheon2750@gmail.com>
2023-08-28 23:05:46 +0900

    Ioc Container와 BEan

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-public class MemoController {
+public class MemoController { //memoController
 
     private final MemoService memoService;
 
--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -4,14 +4,36 @@ import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
 import com.sparta.memo.repository.MemoRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+//@RequiredArgsConstructor //lombok 사용
 public class MemoService {
     private final MemoRepository memoRepository;
+
+
+    //@Autowired 생성자 하나 일때는 생략가능
     public MemoService(MemoRepository memoRepository) {
         this.memoRepository = memoRepository;
     }
+//    public MemoService(ApplicationArguments context){
+            // 1. 'bean' 이름으로 가져오기
+//        MemoRepository memoRepository = (MemoRepository) context.getBean("memoRepository");
+//        this.memoRepository = memoRepository;
+//          // 2. 클래스 로 가져오기
+//        MemoRepository memoRepository = context.getBean(MemoRepository.class);
+//    }
+//    @Autowired // 함수 주입
+//    public void setDi(MemoRepository memoRepository){
+//        this.memoRepository = memoRepository;
+//
+
+//    }
+
+//    @Autowired // 필드 주입
+//    private final MemoRepository memoRepository;
 
     public MemoResponseDto createMemo(MemoRequestDto requestDto) {
         // RequestDto -> Entity
